f27a9e05198edc9546c0e6dffc18bbe47d08831a
Chris Dakin <chris.dakin@civicsource.com>
2019-04-30 10:59:41 -0500

    Prevent string being passed to Object.getOwnProperties

--- src/lazy-fetch.js
+++ src/lazy-fetch.js
@@ -84,10 +84,15 @@ function parseArguments(args) {
 }
 
 function prepareHeaders(itemToFetch) {
-	let { url, bearerToken, ...opts } = itemToFetch || {};
+	let url, bearerToken, opts;
 
-	if (!url && isString(itemToFetch)) {
+	if (isString(itemToFetch)) {
 		url = itemToFetch;
+	} else {
+		const { url: _url, bearerToken: _bearerToken, ..._opts } = itemToFetch || {};
+		url = _url;
+		bearerToken = _bearerToken;
+		opts = _opts;
 	}
 
 	let { headers, ...otherOpts } = opts;
